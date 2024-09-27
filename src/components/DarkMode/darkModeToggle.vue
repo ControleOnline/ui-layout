cc8f4500d0f3f309a00d1a5ed0437a537026e6ad
Luiz Kim <luiz.kim@controleonline.com>
2024-09-27 18:26:03 -0300

    Update

--- src/components/DarkMode/darkModeToggle.vue
+++ src/components/DarkMode/darkModeToggle.vue
@@ -1,33 +1,22 @@
 <template>
-  <q-toggle size="lg" v-model="darkMode" unchecked-icon="light_mode" icon="nightlight" />
+  <q-toggle
+    size="lg"
+    v-model="darkMode"
+    unchecked-icon="light_mode"
+    icon="nightlight"
+  />
 </template>
 
 <script>
-import Config from "@controleonline/ui-common/src/utils/config";
-
 export default {
-  components: {
-    Config,
-  },
-
   data() {
     return {
-      darkMode: false,
-      config: new Config(),
+      darkMode: this.$copyObject(this.$q.dark.isActive),
     };
   },
-
-  created() {
-    let mediaQueryObj = window.matchMedia("(prefers-color-scheme: dark)");
-    let isDarkMode = mediaQueryObj.matches;
-    let darkMode = this.config.getConfig("darkMode");
-    this.darkMode = darkMode == undefined ? isDarkMode : darkMode;
-  },
-
   watch: {
     darkMode(darkMode) {
       this.$q.dark.set(darkMode);
-      this.config.setConfig("darkMode", darkMode);
     },
   },
 };
