51bf26a773d208caf8e95fc4db0ffd613ef4750d
Luiz Kim <luiz.kim@controleonline.com>
2025-02-08 13:50:51 -0300

    Update

--- src/store/theme/mutations.js
+++ src/store/theme/mutations.js
@@ -1,13 +1,11 @@
 import * as types from "./mutation_types";
 
 export default {
-  [types.SET_COLORS](state, payload) {
-    if (!payload?.colors) Object.assign(state, { colors: payload });
-    return { ...state, colors: payload?.colors || payload };
+  [types.SET_COLORS](state, colors) {
+    state.colors = colors;
   },
 
-  [types.SET_MENUS](state, payload) {
-    if (!payload?.menus) Object.assign(state, { menus: payload });
-    return { ...state, menus: payload?.menus || payload };
+  [types.SET_MENUS](state, menus) {
+    state.menus = menus;
   },
 };
