026312a083ef1f802cecfa821f2a413a831edb89
Luiz Kim <luiz.kim@controleonline.com>
2025-03-03 14:40:10 -0300

    Update

---
+++ src/components/Header.vue
@@ -0,0 +1,350 @@
+<template>
+  <q-header elevated height-hint="64">
+    <q-toolbar class="GPL__toolbar" style="height: 64px">
+      <q-btn
+        v-if="defaultCompany.domainType == 'ERP'"
+        flat
+        dense
+        round
+        @click="leftDrawerOpen = !leftDrawerOpen"
+        aria-label="Menu"
+        icon="menu"
+        class="q-mx-md menu-button"
+      />
+      <div class="q-gutter-sm items-center row logo-container">
+        <router-link
+          v-if="
+            defaultCompany.domainType == 'SHOP' &&
+            defaultCompany &&
+            defaultCompany.logo
+          "
+          v-bind:to="'/'"
+          tag="a"
+        >
+          <img
+            :src="'//' + defaultCompany.logo.domain + defaultCompany.logo.url"
+            class="current-logo"
+          />
+        </router-link>
+        <router-link v-if="myCompany && myCompany.logo" v-bind:to="'/'" tag="a">
+          <img
+            :src="'//' + myCompany.logo.domain + myCompany.logo.url"
+            class="current-logo"
+          />
+        </router-link>
+      </div>
+      <div class="q-gutter-sm row items-center no-wrap current-logo-container">
+        <q-toolbar class="">
+          <MyCompanies />
+          <div class="company-title" v-if="this.$q.screen.gt.sm">
+            {{ myCompany.alias }}
+          </div>
+        </q-toolbar>
+      </div>
+      <div class="q-gutter-sm row items-center no-wrap current-user-container">
+        <Notifications />
+        <q-btn icon="account_circle" flat round>
+          <q-tooltip>{{ $tt("menu", "configs", "myAccount") }}</q-tooltip>
+          <q-menu>
+            <div class="row no-wrap q-pa-md">
+              <div class="column">
+                <div class="text-h6 q-mb-md text-center">
+                  {{ $tt("menu", "configs", "myAccount") }}
+                </div>
+                <q-list>
+                  <q-item :to="{ name: 'UserProfile' }" exact>
+                    <q-item-section avatar>
+                      <q-icon name="face" />
+                    </q-item-section>
+                    <q-item-section side>
+                      <q-item-label>{{
+                        $tt("menu", "configs", "myProfile")
+                      }}</q-item-label>
+                    </q-item-section>
+                  </q-item>
+                  <q-item :to="{ name: 'CompanyIndex' }" exact>
+                    <q-item-section avatar>
+                      <q-icon name="business" />
+                    </q-item-section>
+                    <q-item-section side>
+                      <q-item-label>{{
+                        $tt("menu", "configs", "myCompany")
+                      }}</q-item-label>
+                    </q-item-section>
+                  </q-item>
+                  <q-item
+                    v-if="user?.isSuperAdmin"
+                    :to="{ name: 'ConfigsIndex' }"
+                    exact
+                  >
+                    <q-item-section avatar>
+                      <q-icon name="settings" />
+                    </q-item-section>
+                    <q-item-section side>
+                      <q-item-label>{{
+                        $tt("menu", "configs", "configs")
+                      }}</q-item-label>
+                    </q-item-section>
+                  </q-item>
+                  <q-item exact>
+                    <q-item-section avatar>
+                      <router-link
+                        v-if="user?.isSuperAdmin"
+                        :to="{ name: 'TranslateIndex' }"
+                      >
+                        <q-icon name="g_translate" />
+                      </router-link>
+                      <q-icon v-else name="g_translate" />
+                    </q-item-section>
+                    <q-item-section side>
+                      <Language />
+                    </q-item-section>
+                  </q-item>
+
+                  <div class="text-body2 text-center"></div>
+                </q-list>
+              </div>
+              <q-separator vertical inset class="q-mx-lg" />
+              <div class="column items-stretch justify-between">
+                <div class="text-center">
+                  <q-avatar size="64px">
+                    <q-img
+                      :src="
+                        this.$auth.user.avatar
+                          ? this.$auth.user.avatar.domain +
+                            this.$auth.user.avatar.url
+                          : gravatar
+                      "
+                    />
+                  </q-avatar>
+                </div>
+                <div class="text-body2 text-center">
+                  {{ this.$auth.user.realname || "John Doe" }}
+                </div>
+                <div class="text-body2 text-center">
+                  <DarkMode />
+                </div>
+
+                <q-btn
+                  v-close-popup
+                  color="primary"
+                  :label="$tt('menu', 'configs', 'Logout')"
+                  size="sm"
+                  @click="onLogout"
+                />
+              </div>
+            </div>
+          </q-menu>
+        </q-btn>
+      </div>
+    </q-toolbar>
+  </q-header>
+
+  <q-drawer
+    v-if="defaultCompany.domainType == 'ERP'"
+    v-model="leftDrawerOpen"
+    :width="270"
+  >
+    <q-scroll-area class="fit">
+      <q-toolbar class="q-pa-md">
+        <q-toolbar-title class="text-center">
+          <q-avatar size="100px" class="vertical-middle menu-avatar">
+            <router-link v-bind:to="'/'" tag="a" class="primary">
+              <img
+                v-if="defaultCompany.logo"
+                :src="
+                  '//' + defaultCompany.logo.domain + defaultCompany.logo.url
+                "
+                class="q-pa-sm main-logo"
+              />
+            </router-link>
+          </q-avatar>
+        </q-toolbar-title>
+      </q-toolbar>
+
+      <div class="q-pt-md q-px-sm column">
+        <q-list padding>
+          <q-item
+            v-if="isSimple() == false"
+            v-ripple
+            clickable
+            class="GNL__drawer-item"
+            @click="leftDrawerOpen != leftDrawerOpen"
+            :to="{ name: 'DashboardIndex' }"
+          >
+            <q-item-section avatar>
+              <q-icon name="home" />
+            </q-item-section>
+            <q-item-section>
+              {{ $t("menu.dashboard") }}
+            </q-item-section>
+          </q-item>
+          <q-separator inset class="q-my-sm" />
+          <Menu
+            v-if="isSimple() != true"
+            :context="'super_admin'"
+            :people="this.$auth.user"
+            @clickmenu="onClickmenu"
+          />
+        </q-list>
+      </div>
+    </q-scroll-area>
+  </q-drawer>
+  <q-scroll-observer horizontal @scroll="onScroll"></q-scroll-observer>
+</template>
+
+<script>
+import Menu from "@controleonline/ui-common/src/components/Common/Menu";
+import Header from "@controleonline/ui-layout/src/components/Header.vue";
+import MyCompanies from "@controleonline/ui-common/src/components/Common/MyCompanies";
+import Notifications from "@controleonline/ui-common/src/components/Common/Notifications.vue";
+import DarkMode from "@controleonline/ui-layout/src/components/DarkMode/darkModeToggle.vue";
+import Language from "@controleonline/ui-common/src/components/Language/languageToogle.vue";
+import md5 from "md5";
+import { LocalStorage } from "quasar";
+import { mapActions, mapGetters } from "vuex";
+import Company from "@controleonline/ui-common/src/components/Common/Company.vue";
+export default {
+  name: "AdminLayout",
+
+  components: {
+    Menu,
+    MyCompanies,
+    DarkMode,
+    Language,
+    Notifications,
+    Company,
+    Header,
+  },
+
+  data() {
+    return {
+      notifications: {
+        count: 0,
+      },
+
+      disabled: false,
+
+      pageLoading: true,
+      leftDrawerOpen: false, //this.$q.screen.gt.sm,
+      route: {
+        color: "var(--secondary)",
+      },
+    };
+  },
+
+  created() {
+    this.init();
+  },
+
+  computed: {
+    ...mapGetters({
+      defaultCompany: "people/defaultCompany",
+      isLoading: "people/isLoading",
+      myCompany: "people/currentCompany",
+      companies: "people/companies",
+    }),
+    menus() {
+      return this.$store.getters["theme/menus"];
+    },
+
+    style() {
+      return "background: #182840";
+    },
+    gravatar() {
+      if (this.$auth.user.email === undefined) {
+        return "";
+      }
+      return `https://www.gravatar.com/avatar/${md5(
+        this.$auth.user.email
+      )}?s=400`;
+    },
+  },
+
+  watch: {
+    isLoading(isLoading) {
+      if (isLoading) this.$q.loading.show();
+      else this.$q.loading.hide();
+    },
+    defaultCompany(data) {
+      this.verifyPermissions();
+    },
+    menus() {
+      this.getRouteFromMenu(this.$route.name);
+    },
+    $route() {
+      this.route = {
+        color: "var(--header-primary)",
+      };
+      this.getRouteFromMenu(this.$route.name);
+    },
+  },
+
+  methods: {
+    ...mapActions({
+      getCompanies: "people/myCompanies",
+      getRoute: "routes/getItems",
+    }),
+    onScroll(info) {
+      if (info.position > 0) this.leftDrawerOpen = false;
+    },
+    onClickmenu() {
+      this.leftDrawerOpen = !this.leftDrawerOpen;
+    },
+    init() {
+      this.getRouteFromMenu(this.$route.name);
+    },
+    isSimple() {
+      return this.defaultCompany.domainType === "simple";
+    },
+    getRouteFromMenu(routeName) {
+      if (!Array.isArray(this.menus)) return;
+      this.menus.forEach((category) => {
+        if (Array.isArray(category.menus)) {
+          const foundMenu = category.menus.find((m) => m.route === routeName);
+          if (foundMenu) {
+            this.route = foundMenu;
+            this.$store.dispatch("configs/currentModule", this.route.module);
+            return;
+          }
+        }
+      });
+
+      if (!this.route.name)
+        this.getRoute({ route: routeName }).then((result) => {
+          if (result?.lenght > 0) {
+            this.route = result[0];
+            this.$store.dispatch(
+              "configs/currentModule",
+              this.route.module.replace(/\D/g, "")
+            );
+          }
+        });
+    },
+
+    onLogout() {
+      this.$auth.logout();
+    },
+  },
+};
+</script>
+
+<style lang="sass" scoped>
+.company-title
+  margin-left: 20px
+.logo-container
+  width: 100%
+.current-logo
+  display: block
+  margin: auto
+  margin-top: 3px
+  max-height: 100%
+  max-width: 140px
+.logo-container a
+  margin: auto
+.main-logo
+  display: block
+  margin-top: 3px
+  max-width: 100%
+  max-height: 100%
+</style>
--- src/layouts/AdminLayout.vue
+++ src/layouts/AdminLayout.vue
@@ -1,248 +1,16 @@
 <template>
-  <div v-if="pageLoading" class="row">
+  <div v-if="isLoading" class="row">
     <div class="col-12 pageloader">
       <q-spinner color="primary" class="q-uploader__spinner" />
     </div>
   </div>
   <q-layout
-    v-else-if="this.$auth.user.isAdmin && !disabled"
+    v-else-if="isAdmin && !disabled"
     view="lHh Lpr fff"
     class="bg-image"
   >
-    <q-header elevated height-hint="64">
-      <q-toolbar class="GPL__toolbar" style="height: 64px">
-        <q-btn
-          flat
-          dense
-          round
-          @click="leftDrawerOpen = !leftDrawerOpen"
-          aria-label="Menu"
-          icon="menu"
-          class="q-mx-md menu-button"
-        />
-        <div class="q-gutter-sm items-center row logo-container">
-          <router-link
-            v-if="myCompany && myCompany.logo"
-            v-bind:to="'/'"
-            tag="a"
-          >
-            <img
-              :src="'//' + myCompany.logo.domain + myCompany.logo.url"
-              class="current-logo"
-            />
-          </router-link>
-        </div>
-        <div
-          class="q-gutter-sm row items-center no-wrap current-logo-container"
-        >
-          <q-toolbar class="">
-            <MyCompanies />
-            <div class="company-title" v-if="this.$q.screen.gt.sm">
-              {{ myCompany.alias }}
-            </div>
-          </q-toolbar>
-        </div>
-        <!--
-        <div v-if="this.$q.screen.gt.sm" class="q-gutter-sm items-center row">
-          <q-item
-            v-ripple
-            :style="{
-              'font-size': '28px',
-              'font-weight': '500',
-              color: route.color || 'var(--secondary)',
-            }"
-          >
-            <q-item-section
-              avatar
-              v-if="route"
-              :style="{ color: route.color || 'var(--secondary)' }"
-            >
-              <q-icon :name="route.icon" size="36px" />
-            </q-item-section>
-            <q-item-section no-wrap>
-              <q-item-label class="module-tittle">{{
-                $tt("route", "title", this.$route.name)
-              }}</q-item-label>
-            </q-item-section>
-          </q-item>
-        </div>
-      -->
-        <div
-          class="q-gutter-sm row items-center no-wrap current-user-container"
-        >
-          <Notifications />
-          <q-btn icon="account_circle" flat round>
-            <q-tooltip>{{ $tt("menu", "configs", "myAccount") }}</q-tooltip>
-            <q-menu>
-              <div class="row no-wrap q-pa-md">
-                <div class="column">
-                  <div class="text-h6 q-mb-md text-center">
-                    {{ $tt("menu", "configs", "myAccount") }}
-                  </div>
-                  <q-list>
-                    <q-item :to="{ name: 'UserProfile' }" exact>
-                      <q-item-section avatar>
-                        <q-icon name="face" />
-                      </q-item-section>
-                      <q-item-section side>
-                        <q-item-label>{{
-                          $tt("menu", "configs", "myProfile")
-                        }}</q-item-label>
-                      </q-item-section>
-                    </q-item>
-                    <q-item :to="{ name: 'CompanyIndex' }" exact>
-                      <q-item-section avatar>
-                        <q-icon name="business" />
-                      </q-item-section>
-                      <q-item-section side>
-                        <q-item-label>{{
-                          $tt("menu", "configs", "myCompany")
-                        }}</q-item-label>
-                      </q-item-section>
-                    </q-item>
-                    <q-item
-                      v-if="user?.isSuperAdmin"
-                      :to="{ name: 'ConfigsIndex' }"
-                      exact
-                    >
-                      <q-item-section avatar>
-                        <q-icon name="settings" />
-                      </q-item-section>
-                      <q-item-section side>
-                        <q-item-label>{{
-                          $tt("menu", "configs", "configs")
-                        }}</q-item-label>
-                      </q-item-section>
-                    </q-item>
-                    <q-item exact>
-                      <q-item-section avatar>
-                        <router-link
-                          v-if="user?.isSuperAdmin"
-                          :to="{ name: 'TranslateIndex' }"
-                        >
-                          <q-icon name="g_translate" />
-                        </router-link>
-                        <q-icon v-else name="g_translate" />
-                      </q-item-section>
-                      <q-item-section side>
-                        <Language />
-                      </q-item-section>
-                    </q-item>
-
-                    <div class="text-body2 text-center"></div>
-                  </q-list>
-                </div>
-                <q-separator vertical inset class="q-mx-lg" />
-                <div class="column items-stretch justify-between">
-                  <div class="text-center">
-                    <q-avatar size="64px">
-                      <q-img
-                        :src="
-                          this.$auth.user.avatar
-                            ? this.$auth.user.avatar.domain +
-                              this.$auth.user.avatar.url
-                            : gravatar
-                        "
-                      />
-                    </q-avatar>
-                  </div>
-                  <div class="text-body2 text-center">
-                    {{ this.$auth.user.realname || "John Doe" }}
-                  </div>
-                  <div class="text-body2 text-center">
-                    <DarkMode />
-                  </div>
-
-                  <q-btn
-                    v-close-popup
-                    color="primary"
-                    :label="$tt('menu', 'configs', 'Logout')"
-                    size="sm"
-                    @click="onLogout"
-                  />
-                </div>
-              </div>
-            </q-menu>
-          </q-btn>
-        </div>
-      </q-toolbar>
-    </q-header>
-
-    <q-drawer v-model="leftDrawerOpen" :width="270">
-      <q-scroll-area class="fit">
-        <q-toolbar class="q-pa-md">
-          <q-toolbar-title class="text-center">
-            <q-avatar size="100px" class="vertical-middle menu-avatar">
-              <router-link v-bind:to="'/'" tag="a" class="primary">
-                <img
-                  v-if="defaultCompany.logo"
-                  :src="
-                    '//' + defaultCompany.logo.domain + defaultCompany.logo.url
-                  "
-                  class="q-pa-sm main-logo"
-                />
-              </router-link>
-            </q-avatar>
-          </q-toolbar-title>
-        </q-toolbar>
-
-        <div class="q-pt-md q-px-sm column">
-          <q-list padding>
-            <q-item
-              v-if="isSimple() == false"
-              v-ripple
-              clickable
-              class="GNL__drawer-item"
-              @click="leftDrawerOpen != leftDrawerOpen"
-              :to="{ name: 'DashboardIndex' }"
-            >
-              <q-item-section avatar>
-                <q-icon name="home" />
-              </q-item-section>
-              <q-item-section>
-                {{ $t("menu.dashboard") }}
-              </q-item-section>
-            </q-item>
-            <q-separator inset class="q-my-sm" />
-            <Menu
-              v-if="isSimple() != true"
-              :context="'super_admin'"
-              :people="this.$auth.user"
-              @clickmenu="onClickmenu"
-            />
-          </q-list>
-        </div>
-      </q-scroll-area>
-    </q-drawer>
+    <Header />
     <q-page-container class="GPL__page-container">
-      <q-scroll-observer horizontal @scroll="onScroll"></q-scroll-observer>
-
-      <!--
-      <div
-        v-if="menus && route"
-        class="title-page"
-        :style="{
-          'font-size': '28px',
-          'font-weight': '500',
-          color: route.color || 'var(--secondary)',
-        }"
-      >
-        <q-item v-ripple>
-          <q-item-section
-            avatar
-            v-if="route"
-            :style="{ color: route.color || 'var(--secondary)' }"
-          >
-            <q-icon :name="route.icon" size="36px" />
-          </q-item-section>
-          <q-item-section no-wrap>
-         d   {{ $tt("route", "title", this.$route.name) }}d
-          </q-item-section>
-        </q-item>
-
-      </div>
-    -->
-
       <router-view />
     </q-page-container>
   </q-layout>
@@ -255,199 +23,57 @@
   </div>
   <div v-else class="row">
     <div class="col-12 pageloader">
-      <Company @saved="companyCreated" />
+      <Company />
     </div>
   </div>
 </template>
 
 <script>
-import Menu from "@controleonline/ui-common/src/components/Common/Menu";
+import Header from "@controleonline/ui-layout/src/components/Header.vue";
 import MyCompanies from "@controleonline/ui-common/src/components/Common/MyCompanies";
-import Notifications from "@controleonline/ui-common/src/components/Common/Notifications.vue";
-import DarkMode from "@controleonline/ui-layout/src/components/DarkMode/darkModeToggle.vue";
-import Language from "@controleonline/ui-common/src/components/Language/languageToogle.vue";
-import acl from "@controleonline/ui-common/src/utils/acl";
-import md5 from "md5";
-import { LocalStorage } from "quasar";
 import { mapActions, mapGetters } from "vuex";
 import Company from "@controleonline/ui-common/src/components/Common/Company.vue";
 export default {
   name: "AdminLayout",
 
   components: {
-    Menu,
     MyCompanies,
-    DarkMode,
-    Language,
-    Notifications,
     Company,
+    Header,
   },
 
   data() {
-    return {
-      companies: [],
-      notifications: {
-        count: 0,
-      },
-      ACL: new acl(this.$route),
-      disabled: false,
-      permissions: [],
-      pageLoading: true,
-      leftDrawerOpen: false, //this.$q.screen.gt.sm,
-      route: {
-        color: "var(--secondary)",
-      },
-    };
+    return {};
   },
-
-  created() {
-    this.init();
-  },
-
   computed: {
     ...mapGetters({
-      defaultCompany: "people/defaultCompany",
-      isLoading: "people/isLoading",
-      myCompany: "people/currentCompany",
+      companies: "people/companies",
     }),
-    menus() {
-      return this.$store.getters["theme/menus"];
+    isLoading() {
+      return this.$store.getters["acl/isLoading"];
     },
-
-    style() {
-      return "background: #182840";
+    isAdmin() {
+      return this.$store.getters["acl/isAdmin"];
     },
-    gravatar() {
-      if (this.$auth.user.email === undefined) {
-        return "";
-      }
-      return `https://www.gravatar.com/avatar/${md5(
-        this.$auth.user.email
-      )}?s=400`;
+    disabled() {
+      return this.$store.getters["acl/disabled"];
     },
   },
-
+  created() {},
   watch: {
+    /* 
     isLoading(isLoading) {
       if (isLoading) this.$q.loading.show();
       else this.$q.loading.hide();
     },
-    defaultCompany(data) {
-      this.verifyPermissions();
-    },
-    menus() {
-      this.getRouteFromMenu(this.$route.name);
-    },
-    $route() {
-      this.route = {
-        color: "var(--header-primary)",
-      };
-      this.getRouteFromMenu(this.$route.name);
+    */
+    isAdmin(isAdmin) {
+      console.log("isAdmin", isAdmin);
     },
   },
 
   methods: {
-    ...mapActions({
-      getCompanies: "people/myCompanies",
-      getRoute: "routes/getItems",
-    }),
-    onClickmenu() {
-      this.leftDrawerOpen = !this.leftDrawerOpen;
-    },
-    companyCreated() {
-      this.init();
-    },
-    init() {
-      this.discoveryMyCompanyies();
-      this.getRouteFromMenu(this.$route.name);
-    },
-    onScroll(info) {
-      if (info.position > 0) this.leftDrawerOpen = false;
-    },
-    isSimple() {
-      return this.defaultCompany.domainType === "simple";
-    },
-    getRouteFromMenu(routeName) {
-      if (!Array.isArray(this.menus)) return;
-      this.menus.forEach((category) => {
-        if (Array.isArray(category.menus)) {
-          const foundMenu = category.menus.find((m) => m.route === routeName);
-          if (foundMenu) {
-            this.route = foundMenu;
-            this.$store.dispatch("configs/currentModule", this.route.module);
-            return;
-          }
-        }
-      });
-
-      if (!this.route.name)
-        this.getRoute({ route: routeName }).then((result) => {
-          if (result?.lenght > 0) {
-            this.route = result[0];
-            this.$store.dispatch(
-              "configs/currentModule",
-              this.route.module.replace(/\D/g, "")
-            );
-          }
-        });
-    },
-    verifyPermissions() {
-      let user = this.$copyObject(this.$auth.user);
-      this.companies.forEach((company) => {
-        company?.permission?.forEach((item) => {
-          if (this.permissions.indexOf(item) === -1) {
-            this.permissions.push(item);
-            if (
-              item.indexOf("franchisee") !== -1 ||
-              item.indexOf("salesman") !== -1 ||
-              item.indexOf("super") !== -1 ||
-              item.indexOf("admin") !== -1
-            ) {
-              user.isAdmin = true;
-            }
-            if (item.indexOf("super") !== -1) {
-              user.isSuperAdmin = true;
-            }
-            this.$auth.persist(user);
-          }
-        });
-      });
-      this.discoveryIfEnabled();
-    },
-    discoveryIfEnabled() {
-      if (this.companies) {
-        let disabled = true;
-        let user_disabled = true;
-        this.companies.forEach((company) => {
-          user_disabled = !company.user.enabled;
-          if (
-            company.enabled &&
-            company.user.employee_enabled &&
-            !user_disabled
-          ) {
-            if (!this.myCompany)
-              this.$store.dispatch("people/currentCompany", company);
-            disabled = false;
-          }
-          company.permission.forEach((item) => {
-            if (this.permissions.indexOf(item) === 1) {
-              this.permissions.push(item);
-            }
-          });
-        });
-        this.disabled = user_disabled || disabled;
-      }
-      this.pageLoading = false;
-    },
-    discoveryMyCompanyies() {
-      this.getCompanies()
-        .then((response) => {
-          this.companies = response.data;
-        })
-        .finally(() => {
-          this.verifyPermissions();
-        });
-    },
+    ...mapActions({}),
     onLogout() {
       this.$auth.logout();
     },
@@ -456,27 +82,10 @@ export default {
 </script>
 
 <style lang="sass" scoped>
-.company-title
-  margin-left: 20px
 .pageloader
   position: relative
   text-align: center
   margin-top: 300px
-.logo-container
-  width: 100%
-.current-logo
-  display: block
-  margin: auto
-  margin-top: 3px
-  max-height: 100%
-  max-width: 140px
-.logo-container a
-  margin: auto
-.main-logo
-  display: block
-  margin-top: 3px
-  max-width: 100%
-  max-height: 100%
 .bg-image
   background-position: center !important
   background-repeat: no-repeat !important
