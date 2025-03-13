15ae72628611f66bb4477d6788dabcd04f9b52d9
Luiz Kim <luiz.kim@controleonline.com>
2025-03-13 15:45:34 -0300

    Update

---
+++ src/react/components/StateStore.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import globalStyles from '@controleonline/ui-shop/src/react/styles/global';
+import {useTheme} from '@controleonline/ui-layout/src/react/components/ThemeProvider';
+import {StyleSheet, View, Text, ActivityIndicator} from 'react-native';
+import {getStore} from '@store';
+
+const StateStore = ({store}) => {
+  const {getters} = getStore(store);
+  const {item, items, isLoading, error} = getters;
+  const {colors} = useTheme();
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      padding: 10,
+      backgroundColor: '#fff',
+    },
+  });
+
+  if (isLoading)
+    return (
+      <View style={styles.container}>
+        <View style={[styles.content, globalStyles.loadingContainer]}>
+          <ActivityIndicator size="large" color={colors['primary']} />
+        </View>
+      </View>
+    );
+  if (error)
+    <View style={styles.container}>
+      <View style={[styles.content, globalStyles.loadingContainer]}>
+        <Text style={styles.errorText}>{error}</Text>
+      </View>
+    </View>;
+
+  if (!item && (!items || items.length == 0))
+    return (
+      <View style={styles.container}>
+        <View style={[styles.content, globalStyles.loadingContainer]}>
+          <Text style={styles.errorText}>Não encontrado</Text>
+        </View>
+      </View>
+    );
+};
+
+export default StateStore;
