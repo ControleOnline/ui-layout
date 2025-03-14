ef0767f45a3a192a57202fadaeed1190a318092e
BrunoGaffo <bgaffo@gmail.com>
2025-03-14 13:56:37 -0300

    Update

--- src/vue/App.vue
+++ src/vue/App.vue
@@ -12,8 +12,8 @@ import { mapActions, mapGetters } from "vuex";
 import Config from "@controleonline/ui-common/src/utils/config";
 import { APP_ENV } from "@controleonline/../../config/env.js";
 import Acl from "@controleonline/ui-common/src/utils/acl.js";
-import { localStorage } from 'quasar';
-window.localStorage = localStorage;
+import { LocalStorage } from 'quasar';
+window.localStorage = LocalStorage;
 export default {
   components: {
     Analytics,
