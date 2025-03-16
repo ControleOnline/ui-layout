c5b4dd64cb0170dd78974eb223ee6526af9af0c0
Luiz Kim <luiz.kim@controleonline.com>
2025-03-16 00:36:09 -0300

    Update

---
+++ src/react/layouts/CartLayout.js
@@ -0,0 +1,19 @@
+import React, {useState, useEffect} from 'react';
+import {View, StyleSheet, ActivityIndicator, Text} from 'react-native';
+import BottomCart from '@controleonline/ui-orders/src/react/components/cart/BottomCart';
+
+const CartLayout = ({children, navigation, route}) => {
+  return (
+    <View style={styles.container}>
+      <View style={[styles.content]}>{children}</View>
+      <BottomCart navigation={navigation} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {flex: 1},
+  content: {flex: 1},
+});
+
+export default CartLayout;
