9a352a5d0a58fe9fd6c7c8bf28c88c1898bcda2a
Luiz Kim <luiz.kim@controleonline.com>
2025-04-13 21:22:07 -0300

    Update

--- src/react/layouts/ManagerLayout.js
+++ src/react/layouts/ManagerLayout.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import {View, StyleSheet, ActivityIndicator, Text} from 'react-native';
 import BottomToolbar from '@controleonline/ui-manager/src/react/components/BottomToolbar';
+import CompanyFilter from '@controleonline/ui-manager/src/react/components/CompanyFilter';
 
 const ManagerLayout = ({children, navigation, route}) => {
   return (
     <View style={styles.container}>
+      <CompanyFilter navigation={navigation} />
       <View style={[styles.content]}>{children}</View>
       <BottomToolbar navigation={navigation} />
     </View>
