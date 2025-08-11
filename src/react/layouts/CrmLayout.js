7d72605d47fba59c95ef51bb33ababceedb9d0f2
JorgeCastroo <jorgeeduardo27007@gmail.com>
2025-08-11 19:21:38 -0300

    feat:v1

--- src/react/layouts/CrmLayout.js
+++ src/react/layouts/CrmLayout.js
@@ -3,19 +3,25 @@ import {View, StyleSheet} from 'react-native';
 import BottomToolbar from '@controleonline/ui-crm/src/react/components/BottomToolbar';
 import CompanyFilter from '@controleonline/ui-manager/src/react/components/CompanyFilter';
 
-const CrmLayout = ({children, navigation, route}) => {
+const CrmLayout = ({children, navigation}) => {
   return (
     <View style={styles.container}>
-      <CompanyFilter navigation={navigation} />
-      <View style={[styles.content]}>{children}</View>
+      <CompanyFilter />
+      <View style={styles.content}>{children}</View>
       <BottomToolbar navigation={navigation} />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {flex: 1},
-  content: {flex: 1},
+  container: {
+    flex: 1,
+    backgroundColor: '#f8f9fa',
+  },
+  content: {
+    flex: 1,
+    backgroundColor: '#f8f9fa',
+  },
 });
 
 export default CrmLayout;
